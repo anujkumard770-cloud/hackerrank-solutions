c7e5f01ca7895f381110d2712d64f8e336567d7f
Anuj Kumar Dubey <anujkumard770@gmail.com>
2026-09-18 11:13:17 +0530

    ✅ Classes and Objects [cpp] — approach1

---
+++ solutions/cpp/easy/classes-objects/solution-approach1.cpp
@@ -0,0 +1,36 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/classes-objects/problem?isFullScreen=true
+// Problem     Classes and Objects
+// Difficulty  Easy
+// Subdomain   Classes
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-09-18, 11:13 a.m.
+// ──────────────────────────────────────────────────
+
+
+
+// Write your Student class here
+class Student{
+    public:
+      int scores[5];
+      
+      
+      void input(){
+        for(int i=0; i<5; i++){
+            cin>>scores[i];
+        }
+      }
+      
+      int calculateTotalScore(){
+        int total=0;
+        
+        for(int i=0; i<5; i++){
+            total += scores[i];
+        }
+        
+        return total;
+      }
+};
+
